6a5907765ae457dc5987b387182777575083de2a
matchakay <zveupmjsgv@gmail.com>
2019-03-22 00:15:39 +0900

    edit

--- test/models/art_category_test.rb
+++ test/models/art_category_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ArtCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "ArtCategory save" do
+    ArtCategory = ArtCategory.new ({
+        id: 1,
+        name: "金製品",
+    })
+    assert ArtCategory.save, 'Succeeded to save'
+  end
 end
